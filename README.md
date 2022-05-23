7fac187ec1112e8fede3328b6c9af5e18388a98e
ahmedshindy <ahmedshindy70@gmail.com>
2022-05-24 00:30:30 +0200

    organizing drivers

---
+++ README.md
@@ -0,0 +1,4 @@
+# atmega32_drivers
+
+Some drivers that I have written for atmega32 Avr Microconroler.
+These drivers are written under supervison of Eng [Mohamed Zaghloul](https://www.linkedin.com/in/mohamed-zaghloul-3b75b9148).
---
+++ SPI/spi_driver.h
@@ -0,0 +1,11 @@
+
+
+
+#ifndef SPI_DRIVER_H_
+#define SPI_DRIVER_H_
+void SPI_MasterInit(void);
+void SPI_SlaveInit(void);
+void SPI_MasterTransmitchar(unsigned char Data);
+unsigned char SPI_SlaveReceivechar(unsigned char Data);
+void SPI_MasterTransmitstring(unsigned char *ptr);
+#endif /* SPI_DRIVER_H_ */
